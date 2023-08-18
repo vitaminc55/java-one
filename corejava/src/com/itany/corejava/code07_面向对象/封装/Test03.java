1580049de69667ab53050d9013c4cd47025c39a0
shixiaojun <shixiaojun@itany.com>
2023-08-18 09:13:22 +0800

    提交上课代码

--- corejava/src/com/itany/corejava/code07_面向对象/封装/Test03.java
+++ corejava/src/com/itany/corejava/code07_面向对象/封装/Test03.java
@@ -5,7 +5,6 @@ package com.itany.corejava.code07_面向对象.封装;
  * @date 2023年08月17日 14:25
  */
 public class Test03 {
-
     public static void main(String[] args) {
         System.out.println("****************洗牌前的所有扑克牌****************");
         Poker poker = new Poker();
@@ -13,14 +12,11 @@ public class Test03 {
         for(Card card : cards){
             System.out.print(card.getColor()+card.getNum()+"\t");
         }
-
         System.out.println("\n****************洗牌后的所有扑克牌****************");
         poker.shuffle();
         cards = poker.getCards();
         for(Card card : cards){
             System.out.print(card.getColor()+card.getNum()+"\t");
         }
-
     }
-
 }
