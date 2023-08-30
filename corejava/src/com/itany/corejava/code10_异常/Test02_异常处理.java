2a44c0f4427d7f356187b44d8efc569fceda9581
shixiaojun <shixiaojun@itany.com>
2023-08-30 16:58:38 +0800

    提交上课代码

--- corejava/src/com/itany/corejava/code10_异常/Test02_异常处理.java
+++ corejava/src/com/itany/corejava/code10_异常/Test02_异常处理.java
@@ -1,13 +1,19 @@
 package com.itany.corejava.code10_异常;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * @author 石小俊
  * @date 2023年08月30日 16:10
  */
 public class Test02_异常处理 {
 
-    public static void main(String[] args) {
-        test01();
+    public static void main(String[] args) throws Exception {
+//        test01();
+        test02("aaa");
+//        test03(1,0);
     }
 
     public static void test01() {
@@ -33,6 +39,45 @@ public class Test02_异常处理 {
             System.out.println("不管是否存在异常都会执行的代码");
         }
         System.out.println("这是异常处理语句之后的代码");
+    }
+
+    public static void test02(String s) throws ParseException {
+        try {
+            Date date = new SimpleDateFormat("yyyy-MM-dd").parse(s);
+        } catch (ParseException e) {
+            // 此处表示捕获到对应异常,可以进行处理了
+            System.out.println("日期字符串格式有误");
+            // 在此处也可以通过关键字throw将异常抛出去,不进行处理
+            // throw关键字后面必须跟异常对象
+            // 如果没有异常对象, 则可以通过new 异常类型()来创建一个异常对象
+            // 也可以直接抛出当前捕获到的异常对象
+            // 当通过关键字throw抛出异常时,表示此处并没有对异常处理完成
+            // 因此,在方法的声明处必须通过throws关键字抛出当前所抛出异常
+            throw e;
+        } finally {
+            System.out.println("这是finally");
+        }
+    }
+
+    public static void test03(int i, int j) throws Exception {
+        // 使用throw关键字可以在任意位置进行使用
+        // 并不是必须在异常处理代码中
+        if (j == 0) {
+            // 当除数为0时,会出现算术异常
+            // 此处可以定义一个算术异常对象,通过throw关键字抛出去
+            // ArithmeticException e = new ArithmeticException("第二个参数不能为空,否则无法进行除法运算");
+            // 由于算术异常属于运行时异常,使用throw关键字抛出后可以不在方法声明中通过throws抛出
+            // 只有检查异常必须抛出
+            // throw e;
+
+            // 简写方式
+            // throw new ArithmeticException("第二个参数不能为空,否则无法进行除法运算");
+
+            // 此时可以更换异常类型
+            // 当前抛出的异常属于检查异常,此时的方法声明处必须通过throws抛出
+            throw new Exception("这是检查异常");
+            // throw关键字效果类似于return,表示方法结束,后面不能再有代码了
+        }
     }
 
 }
